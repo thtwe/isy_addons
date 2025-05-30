eb82cb64e397f01020c9ffcfcd6986766fc3378d
Thae Ei Htwe <thtwe@isyedu.org>
2025-05-30 09:29:18 +0630

    add new isy_pos_addon and update code for extended budget view and reimbursement settlement in mt_isy and accounting_budget_extension_V7.

---
+++ isy_pos_addon/__manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': "ISY POS",
+    'version': '17.0.1.0.0',
+    'summary': """
+        Support order lines' note
+    """,
+    'description': """
+        Support order lines' note
+    """,
+    'author': "ISY",
+    'website': "https://www.isyedu.org",
+    'category': 'Point of sale',
+    'depends': ['point_of_sale'],
+    'data': [],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+    'license': 'LGPL-3',
+}
